146cafa5e34c7b0282800355880d4313fd236248
Dineshkarthik <dineshkarthik.r@gmail.com>
2020-06-26 17:04:53 +0200

    Bot initial version

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+}
+TOKEN = os.environ.get("BOT_TOKEN")
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
