ed807499d3a526fe1f87ef971d14ca5fde6a48fa
Dineshkarthik <dineshkarthik.r@gmail.com>
2020-12-18 23:02:36 +0100

    add: setup.py for versioning

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name="pinterest-telegram-bot",
+    version="1.1.0",
+    author="Dineshkarthik Raveendran",
+    author_email="hello@dineshkarthik.me",
+    description="A simple script to download media from telegram",
+    url="https://github.com/Dineshkarthik/pinterest-telegram-bot",
+    download_url="https://github.com/Dineshkarthik/pinterest-telegram-bot/releases/latest",
+    py_modules=["pinterest_telegram_bot"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Communications :: Chat",
+    ],
+    python_requires=">=3.6",
+)
