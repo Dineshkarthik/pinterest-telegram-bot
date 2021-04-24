4b7ad06b738180857a0e1d28918011909cc7e5b9
Dineshkarthik <dineshkarthik.r@gmail.com>
2021-04-24 11:13:25 +0200

    build(package): bump pinterest-telegram-bot from 1.2.0 to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pinterest-telegram-bot",
-    version="1.2.0",
+    version="2.0.0",
     author="Dineshkarthik Raveendran",
     author_email="hello@dineshkarthik.me",
     description="A simple script to download media from telegram",
