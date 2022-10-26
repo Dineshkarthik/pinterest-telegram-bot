f5dc00856542c9a8c712b240ebd0a3808dfacfa5
dineshkarthikr <dineshkarthik.r@gmail.com>
2022-10-26 19:28:08 +0200

    upd: host header

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Host": "www.pinterest.com"
 }
 TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
